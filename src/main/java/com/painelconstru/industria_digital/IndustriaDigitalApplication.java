7a8c3aad61d99bfbba8e71d85d4012fb190af5af
PedroCarlucci <pkcarlucci@gmail.com>
2024-10-04 11:12:53 -0300

    initial commit

---
+++ src/main/java/com/painelconstru/industria_digital/IndustriaDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.painelconstru.industria_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustriaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustriaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/painelconstru/industria_digital/IndustriaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.painelconstru.industria_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustriaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
